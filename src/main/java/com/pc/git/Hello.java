487e72549b2bc2ef83e48ede1669638140ac51e4
pengchao <2389367080@qq.com>
2020-02-19 21:55:07 +0800

    commit push

--- src/main/java/com/pc/git/Hello.java
+++ src/main/java/com/pc/git/Hello.java
@@ -13,11 +13,6 @@ public class Hello {
         System.out.println(22);//master dev commit
         System.out.println(33);
         System.out.println(44);
-        System.out.println(55);
-        System.out.println(66);
-        System.out.println(77);
-        System.out.println(88);
-        System.out.println(99);
 
     }
 }
