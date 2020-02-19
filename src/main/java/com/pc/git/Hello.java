7c82918dac2739d2a651178142cca5ff6b2f537a
pengchao <2389367080@qq.com>
2020-02-19 12:26:24 +0800

    clear

--- src/main/java/com/pc/git/Hello.java
+++ src/main/java/com/pc/git/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println(11);
-        System.out.println(22);// master dev 修改
+        System.out.println(22);
         System.out.println(33);
         System.out.println(44);
 
