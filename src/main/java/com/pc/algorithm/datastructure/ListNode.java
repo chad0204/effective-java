a5df2674e10b7a2d09cc5fdb10f07743dce3eeb6
pengchao <2389367080@qq.com>
2021-01-25 22:18:33 +0800

    list

--- src/main/java/com/pc/algorithm/datastructure/ListNode.java
+++ src/main/java/com/pc/algorithm/datastructure/ListNode.java
@@ -17,12 +17,12 @@ public class ListNode<T> {
         ListNode head = new ListNode<>(1);	//根节点A
 
         head.next = new ListNode<>(2);
-//        head.next.next = new ListNode<>(3);
-//        head.next.next.next = new ListNode<>(4);
-//        head.next.next.next.next = new ListNode<>(5);
-//        head.next.next.next.next.next = new ListNode<>(6);
-//        head.next.next.next.next.next.next = new ListNode<>(7);
-//        head.next.next.next.next.next.next.next = new ListNode<>(8);
+        head.next.next = new ListNode<>(3);
+        head.next.next.next = new ListNode<>(4);
+        head.next.next.next.next = new ListNode<>(5);
+        head.next.next.next.next.next = new ListNode<>(6);
+        head.next.next.next.next.next.next = new ListNode<>(7);
+        head.next.next.next.next.next.next.next = new ListNode<>(8);
         return head;
     }
 
