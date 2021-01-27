4a76f2e6d0e80f8f6c6d8ac70122851afc06993b
pengchao <2389367080@qq.com>
2021-01-27 10:14:58 +0800

    two sum

--- src/main/java/com/pc/algorithm/nsum/TwoSum.java
+++ src/main/java/com/pc/algorithm/nsum/TwoSum.java
@@ -27,6 +27,7 @@ public class TwoSum {
 
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15},9)));
 
         System.out.println(Arrays.toString(twoSumWithIndex(new int[]{3,2,4}, 6)));
         System.out.println(twoSumNoDup(new int[]{1,1,1,2,2,2,3,3,3,}, 4));
@@ -43,8 +44,9 @@ public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
 
         Map<Integer,Integer> map = new HashMap<>();
-        for(int i : nums) {
-            map.put(i,nums[i]);
+        //将值作为key,索引作为value放入map
+        for(int i=0;i<nums.length;i++) {
+            map.put(nums[i],i);
         }
 
         for(int i=0;i<nums.length;i++) {
---
+++ src/main/java/com/pc/algorithm/nsum/readme.md
@@ -0,0 +1,8 @@
+1.两数之和（简单） 
+https://leetcode-cn.com/problems/two-sum/
+
+167.两数之和 II - 输入有序数组（中等）
+https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+
+170.两数之和 III - 数据结构设计（简单）
+https://leetcode-cn.com/problems/two-sum-iii-data-structure-design/
