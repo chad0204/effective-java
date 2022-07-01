cf0767fa503c1d1d542472cf8822a1aeb569010c
pengchao <pengchao@lianlianpay.com>
2022-07-01 21:21:29 +0800

    feat: forkjoin test

--- src/main/java/com/pc/concurrent/future/forkjoin/T.java
+++ src/main/java/com/pc/concurrent/future/forkjoin/T.java
@@ -5,37 +5,7 @@ import java.util.concurrent.*;
 public class T {
 
     public static void main(String[] args) {
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
 
-        forkJoinPool.execute(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        });
-
-        ForkJoinTask forkJoinTask = new ForkJoinTask() {
-            @Override
-            public Object getRawResult() {
-                return null;
-            }
-
-            @Override
-            protected void setRawResult(Object value) {
-
-            }
-
-            @Override
-            protected boolean exec() {
-                return false;
-            }
-        };
-
-        ForkJoinWorkerThread forkJoinWorkerThread;
-
-
-        RecursiveTask recursiveTask;
-        RecursiveAction recursiveAction;
 
 
     }
