4ef80708052bc92ede25c5698e42d53912182abe
pengchao <pengchao@lianlianpay.com>
2022-08-17 15:10:32 +0800

    feat: some util

---
+++ src/main/java/com/pc/util/RangeCommon.java
@@ -0,0 +1,20 @@
+package com.pc.util;
+
+import org.apache.commons.lang3.Range;
+
+/**
+ * TODO
+ *
+ * @author pengchao
+ * @since 2022/7/6 11:35
+ */
+public class RangeCommon {
+
+    public static void main(String[] args) {
+
+
+        Range.between(10,20).contains(1);
+
+
+    }
+}
