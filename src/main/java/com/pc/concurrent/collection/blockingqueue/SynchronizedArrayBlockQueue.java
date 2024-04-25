fee9252a95f5d8f6ea36086a2fa63a45e53f8edb
pengchao <2389367080@qq.com>
2024-04-25 10:48:16 +0800

    feat: concurrent

--- src/main/java/com/pc/concurrent/collection/blockingqueue/SynchronizedArrayBlockQueue.java
+++ src/main/java/com/pc/concurrent/collection/blockingqueue/SynchronizedArrayBlockQueue.java
@@ -14,14 +14,24 @@ public class SynchronizedArrayBlockQueue {
         this.buffer = new String[capacity];
     }
 
+    /**
+     *
+     *  C1
+     *  C2
+     *  P1
+     *  P2
+     *
+     *  C1,C2发现队列为空，调用wait阻塞, 并释放锁
+     *  P1获取锁，put元素，唤醒条件队列中的C1或C2中的一个，但是没有竞争过P1，P1又获取锁，直到队列已满，P1阻塞释放锁
+     *  P2获取锁，发现队列已满，也阻塞。此时P1,P2和C1,C2全部阻塞
+     *
+     */
     public synchronized void put(String ele) throws InterruptedException {
         while (count == buffer.length) {
             System.out.println("队列已满");
             wait();
         }
         buffer[count++] = ele;
-        //唤醒必须用notifyAll(), 否则会死锁
-        //队列为空c0,c1都阻塞, 此时p0执行完毕, notify()只能唤醒一个线程, 如果每次唤醒的还是p0, 直到队列满, p0也阻塞。
         notifyAll();
     }
 
